64886d44b8f2d8d1752863d2d13266703bd3ba30
don wade <don_wade@rogers.com>
2024-03-05 12:13:08 -0500

    scrapping scp/rsh uploader

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+
+# used for uploading an arduino raspi build onto the h/w target.
+
 PI_HOST=$1
-PI_PORT=$2
-PI_PASS=$3
+NOT_USED_PI_PORT=$2
+NOT_USED_PI_PASS=$3
 PI_FILE=$4
 
-if [ $PI_PORT -eq 8266 ]; then
-	PI_PORT=22
-fi
+rm /tmp/sketch.bin
 
-if [ $PI_PASS == "no_pwd_found_in_code" ]; then
-	scp -P $PI_PORT $PI_FILE root@$PI_HOST:/tmp/sketch.bin
-	ssh -p $PI_PORT root@$PI_HOST '/usr/local/bin/merge-sketch-with-bootloader.lua'
-	ssh -p $PI_PORT root@$PI_HOST '/usr/local/bin/run-avrdude'
-else
-	expect -c "spawn scp -P $PI_PORT $PI_FILE root@$PI_HOST:/tmp/sketch.bin; expect \"password:\"; send \"$PI_PASS\\r\"; interact"
-	expect -c "spawn ssh -p $PI_PORT root@$PI_HOST /usr/local/bin/merge-sketch-with-bootloader.lua; expect \"password:\"; send \"$PI_PASS\\r\"; interact"
-	expect -c "spawn ssh -p $PI_PORT root@$PI_HOST /usr/local/bin/run-avrdude; expect \"password:\"; send \"$PI_PASS\\r\"; interact"
-fi
+cp $PI_FILE /tmp/sketch.bin
+touch /tmp/sketch.bin
+rsync /tmp/sketch.bin dwade@$PI_HOST:/tmp/sketch.bin
+
+ssh dwade@$PI_HOST 'ls -al /tmp/sketch.bin'
+echo
+
+ssh dwade@$PI_HOST '/usr/local/bin/merge-sketch-with-bootloader.lua'
+ssh dwade@$PI_HOST '/usr/local/bin/run-avrdude'
+
